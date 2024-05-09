22b4c5313d79d85e8279bbf592a538d5f15a5ea0
MrTussa <79701873+MrTussa@users.noreply.github.com>
2024-05-09 18:44:35 +0500

    Creating project

---
+++ components/Hero.tsx
@@ -0,0 +1,31 @@
+"use client";
+import Image from "next/image";
+import CustomButton from "./CustomButton";
+
+const Hero = () => {
+  const handleScroll = () => {};
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">Найти машину - быстро и легко!</h1>
+        <p className="hero__subtitle">
+          Упростите поиск нужной вам машины при помощи нашего сайта.
+        </p>
+
+        <CustomButton
+          title="Найти машину"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <div className="hero__image-overlay" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
