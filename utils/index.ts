4cf75ea5515cfc9cde84e180c7c5c68d1f8ed0ad
MrTussa <79701873+MrTussa@users.noreply.github.com>
2024-05-16 17:42:58 +0500

    Added: Seach request

--- utils/index.ts
+++ utils/index.ts
@@ -1,16 +1,22 @@
+import { FilterProps } from '@/types';
 import axios from 'axios';
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+  const {manufacturer, year, model, limit, fuel} = filters
       const responce = await axios.request(
         {
             method: 'GET',
             url: 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',
-            params: {model: 'cobalt'},
+            params: {model: model, make:manufacturer, year: year, limit: limit, fuel: fuel},
             headers: {
               'X-RapidAPI-Key': 'e6fcb91f62msh00399125f4d9165p18917fjsn45cd1ef84581',
               'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
             }
         }
       )
+      console.log(manufacturer);
+      
+      // console.log(responce.data);
+      
       return responce.data
 }
