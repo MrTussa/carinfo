df71ca69056d12281d3ed41c4dd5060fee2eea74
MrTussa <dd993736@gmail.com>
2024-07-15 02:26:29 +0500

    Added: Next-vide background

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,7 +4,7 @@ import CustomButton from "./CustomButton";
 
 const NavBar = () => {
   return (
-    <header className="w-full absolute z-10">
+    <header className="w-full absolute z-10 bg-slate-50 opacity-95 shadow-2xl shadow-white box-border">
       <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
         <Link href="/" className="flex justify-center items-center">
           <Image
@@ -15,12 +15,12 @@ const NavBar = () => {
             className="object-contain"
           />
         </Link>
-
+        {/* 
         <CustomButton
           title="Войти"
           btnType="button"
           containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
-        />
+        /> */}
       </nav>
     </header>
   );
