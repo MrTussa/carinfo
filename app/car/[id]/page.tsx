22347c99dd88df09fcb517a0880a886ce544dba1
MrTussa <dd993736@gmail.com>
2024-07-20 02:59:21 +0500

    Updated: car details page

--- app/car/[id]/page.tsx
+++ app/car/[id]/page.tsx
@@ -1,8 +1,22 @@
 import { fetchCarById } from "@/utils";
-
+import { Container } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
 const Page = async ({ params }: { params: { id: string } }) => {
   const carDetails = await fetchCarById(Number(params.id));
-  return <div>{carDetails.price}</div>;
+  return (
+    <Container maxWidth="lg" className="pt-[52px]">
+      <Grid container spacing={1} className=" card__main">
+        <Grid xs={7}>
+          <section>
+            <p>asdasd</p>
+          </section>
+        </Grid>
+        <Grid xs={4}>
+          <section></section>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default Page;
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -6,11 +6,11 @@ const NavBar = () => {
   return (
     <header className="w-full absolute z-10 bg-slate-50 opacity-95 shadow-2xl shadow-white box-border">
       <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
-        <Link href="/" className="flex justify-center items-center">
+        <Link href="/" className="flex h-5 justify-center items-center">
           <Image
             src="/logo.svg"
             alt="Car Info Logo"
-            width={118}
+            width={120}
             height={18}
             className="object-contain"
           />
